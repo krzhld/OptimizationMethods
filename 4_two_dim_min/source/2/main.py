9a22a1e959e0469450b372916ffa66485db14265
Kirill <kirill.zheludev@yandex.ru>
2023-04-15 20:32:48 +0300

    add missing minus

--- 4_two_dim_min/source/2/main.py
+++ 4_two_dim_min/source/2/main.py
@@ -41,8 +41,8 @@ def mult_vectors(v1 : list, v2 : list) -> float:
     return v1[0] * v2[0] + v1[1] * v2[1]
 
 def newton_method(func, grad_func, hess_func, eps):
-    x_k = 1
-    y_k = 1
+    x_k = 15
+    y_k = 2
     alpha_0 = 1
     delta = 0.5 
     lambd = 0.5 
@@ -57,14 +57,14 @@ def newton_method(func, grad_func, hess_func, eps):
         # print(f'{norm_grad}')
         d_k = np.array(mult_matrix_and_vector(np.linalg.inv(hess_func(x_k,y_k)), grad)) 
         alpha_k = 0.01 
-        # alpha_k = alpha_0 
+        alpha_k = alpha_0
 
-        # f = func(x_k - alpha_k * d_k[0], y_k - alpha_k * d_k[1])
-        # f_k = func(x_k, y_k)
-        # while(f - f_k > delta * alpha_k * mult_vectors(grad, d_k)):
-        #     alpha_k *= lambd 
+        f = func(x_k - alpha_k * d_k[0], y_k - alpha_k * d_k[1])
+        f_k = func(x_k, y_k)
+        while(f - f_k > -delta * alpha_k * mult_vectors(grad, d_k)):
+            alpha_k *= lambd
 
-        #     f = func(x_k - alpha_k * d_k[0], y_k - alpha_k * d_k[1])
+            f = func(x_k - alpha_k * d_k[0], y_k - alpha_k * d_k[1])
             
 
         # alpha_0 = alpha_k 
@@ -81,7 +81,7 @@ x, y = np.meshgrid(np.linspace(-4, 1, 1000), np.linspace(-1.5, 1, 1000))
 
 # print(f'[{result[0]}, {result[1]}]')
 
-h = np.array(hess_f(1,1))
+h = np.array(hess_f(1, 1))
 hinv = np.array(np.linalg.inv(h))
 
 print(f'{h}')
@@ -91,7 +91,6 @@ print(f'{h * hinv}')
 z = f(x, y)
 plt.figure()
 plt.contour(x, y, z, 50)
-# result = newton_method(f, grad_f, hess_f, 10e-4)
-# plt.scatter(result[0], result[1])
+result = newton_method(f, grad_f, hess_f, 10e-6)
+plt.scatter(result[0], result[1])
 plt.show()
-
